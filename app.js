a64b860664dc812e5c8f1586e9ddf04395f9f8af
Shivansh Ganguly <shivansh.ganguly.2007@gmail.com>
2025-09-20 09:42:36 +0530

    More Animations

---
+++ app.js
@@ -0,0 +1,29 @@
+const img1 = document.querySelectorAll(".images .img");
+const img2 = document.querySelectorAll(".images2 .img");
+
+img1.forEach(i => {
+    i.addEventListener("mouseover", () => {
+        const parent = i.parentElement;
+        img1.forEach(j => {
+            j.style.animationPlayState = "paused";
+        })
+    })
+    i.addEventListener("mouseout", () => {
+        img1.forEach(j => {
+            j.style.animationPlayState = "running";
+        })
+    })
+});
+img2.forEach(i => {
+    i.addEventListener("mouseover", () => {
+        const parent = i.parentElement;
+        img2.forEach(j => {
+            j.style.animationPlayState = "paused";
+        })
+    })
+    i.addEventListener("mouseout", () => {
+        img2.forEach(j => {
+            j.style.animationPlayState = "running";
+        })
+    })
+});
