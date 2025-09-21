3bc328b2f1aa3e320ddd458619f6f581788f8916
Shivansh Ganguly <shivansh.ganguly.2007@gmail.com>
2025-09-21 09:26:40 +0530

    Formatting Changes

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const img1 = document.querySelectorAll(".images .img");
 const img2 = document.querySelectorAll(".images2 .img");
+const showSpan = false;
 
 img1.forEach(i => {
     i.addEventListener("mouseover", () => {
-        const parent = i.parentElement;
         img1.forEach(j => {
             j.style.animationPlayState = "paused";
         })
+        showSpan = true
+        const span = document.querySelector(`${e.target} span`);
+        span.style.visibility = "visible";
     })
-    i.addEventListener("mouseout", () => {
+    i.addEventListener("mouseout", (e) => {
         img1.forEach(j => {
             j.style.animationPlayState = "running";
         })
+        const span = document.querySelector(`${e.target} span`);
+        span.style.visibility = "hidden";
     })
+    i.addEventListener("click", () => moreInfo(e));
 });
 img2.forEach(i => {
     i.addEventListener("mouseover", () => {
@@ -26,4 +32,12 @@ img2.forEach(i => {
             j.style.animationPlayState = "running";
         })
     })
+    i.addEventListener("click", () => moreInfo(e));
 });
+
+function moreInfo(event) {
+    event.target.style.width = "500px";
+    event.target.style.height = "500px";
+    event.target.style.fontSize = "25px";
+    event.target.style.textAlign = "center";
+}
